b65b591a4d54fffe014dce74fa695b9201bf9ece
vranda-agrawal <vranda.a@simformsolutions.com>
2022-05-18 12:28:06 +0530

    refrac: indented the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname,:phone_number,:age,:role_ids ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname,:phone_number,:age,:role_ids ])
   end
 
   def after_sign_out_path_for(resource_or_scope)
-      home_path
+    home_path
   end
 end
