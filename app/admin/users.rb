2b757b5d1b403c527ca2045862270ee1977df78c
vranda-agrawal <vranda.a@simformsolutions.com>
2022-05-24 13:13:37 +0530

    feat:added admin panel functionality

---
+++ app/admin/users.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register User do
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :fullname
+    column :phone_number
+    column :age
+    actions
+  end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :email, :encrypted_password, :fullname, :phone_number, :age, :reset_password_token, :reset_password_sent_at, :remember_created_at, :role_ids, :password, :password_confirmation
+  
+  form do |f|
+    f.inputs do
+      f.input :fullname
+      f.input :email
+      f.input :phone_number
+      f.input :age
+      f.input :role_ids, :input_html => { :value => 2 }, as: :hidden
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   
   protected
   def configure_permitted_parameters
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+
+  def after_sign_in_path_for(resource)
+    admin_dashboard_path
+  end
+
 end
