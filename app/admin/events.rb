74c797fc4f057025d63ebb3bbb8816996e41750c
vranda-agrawal <vranda.a@simformsolutions.com>
2022-05-24 14:14:08 +0530

    refrac:removed commented lines

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,19 +1,5 @@
 ActiveAdmin.register Event do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :event_name, :description, :event_date, :event_time, :price, :admin_user_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:event_name, :description, :event_date, :event_time, :price, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
   form do |f|
     f.inputs do
