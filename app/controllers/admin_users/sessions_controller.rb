968a5472d1f2f864bb224f488b4242ac55d53535
vranda-agrawal <vranda.a@simformsolutions.com>
2022-05-27 11:17:58 +0530

    feat:added transaction model at admin panel

---
+++ app/controllers/admin_users/sessions_controller.rb
@@ -0,0 +1,12 @@
+class AdminUsers::SessionsController < Devise::SessionsController
+
+  protected
+  def after_sign_in_path_for(resource)
+    admin_dashboard_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_user_session_path
+  end
+
+end
